daa0369c8b641daa11043a2f6949426f8783f56a
SDiza_HomePC <sdshpak@gmail.com>
2019-10-21 22:20:48 +0300

    add price tabs

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,28 @@
 $(function(){
 
+    $('.slider__inner').slick({
+        rows: 2,
+        slidesToShow: 3,
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        infinite: false
+    });
 
 
-  
 });
+
+(function($) {
+    $(function() {
+        $("ul.tabs__caption").on("click", "li:not(.active)", function() {
+            $(this)
+                .addClass("active")
+                .siblings()
+                .removeClass("active")
+                .closest("div.tabs")
+                .find("div.tabs__content")
+                .removeClass("active")
+                .eq($(this).index())
+                .addClass("active");
+        });
+    });
+})(jQuery);
